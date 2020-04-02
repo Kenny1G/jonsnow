7f9c9af711b1c35477e8cd624bf4f86122687beb
kenny <oseleonose@gmail.com>
2020-04-02 13:09:38 -0400

    testing system complete

--- startProcess.cpp
+++ startProcess.cpp
@@ -3,13 +3,12 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include <sstream>
 #include <stdlib.h>
 
 
 int main(int argc, char** argv)
 {
 	std::string command = argv[1];
-	command.append("-i companion.cfg &");
+	command.append(" -i companion.cfg &");
 	int a = system(command.c_str());	
 }
